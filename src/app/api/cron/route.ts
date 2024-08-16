601b8118709e8fdae9fd95479d58a6b17da40e1c
mindangola <mindangola.contacto@gmail.com>
2024-08-16 13:37:22 +0100

    create api route

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,10 +1,12 @@
+import { NextResponse } from "next/server"
+
  
 type ResponseData = {
   message: string
 }
  
-export async function GET(request: Request, res: Response) {
+export async function GET(request: Request) {
   console.log('Cron run on', new Date())
 
-  request.json()
+  return new NextResponse("Job is Running", {status: 200})
 }
